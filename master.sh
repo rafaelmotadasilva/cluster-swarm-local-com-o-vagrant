48b5ad73a88990f0f63090902841424025638916
Rafael Mota <ti.rafaelmota@gmail.com>
2024-01-31 15:02:41 -0300

    dockersamples/visualizer

--- master.sh
+++ master.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 sudo docker swarm init --advertise-addr=172.16.10.100
 sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
+sudo docker service create \
+  --name=viz \
+  --publish=5000:8080/tcp \
+  --constraint=node.role==manager \
+  --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+  dockersamples/visualizer
